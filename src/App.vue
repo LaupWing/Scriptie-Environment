ec1f498451922fb6f4ae10307540ba8463db0787
LaupWing <43813491+LaupWing@users.noreply.github.com>
2022-06-09 09:41:21 +0200

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
 	<div id="app" class="w-screen h-screen text-center flex flex-col items-center overflow-hidden bg-background">
 		<img alt="Vue logo" src="./assets/logo.png">
-		<HelloWorld msg="Welcome to Your Vue.js App"/>
 	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'App',
+	components: {
+	}
 }
 </script>
 
