3fa8af4d957b5faddbcee4ef1ef8e6f5e244321a
LaupWing <loc_strict@hotmail.com>
2022-06-10 13:01:45 +0200

    firbase utils

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,36 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-   modules: {
+   state: {
+      count: 0
    },
-   plugins: []
+   mutations: {
+      increment(state) {
+         state.count++
+      }
+   },
+   actions:{
+      async singleDraft(_, {user_id, type_id, handboek_id, content=true}){
+         try{
+            const snapshot = await handboekenRef(user_id, type_id)
+               .doc(handboek_id)
+               .get()
+      
+            const handboek = snapshot.data()
+            let content_versions = handboek.content_versions
+            if(content){
+               content_versions = await combineVersionsWithStorage(
+                  `handboeken_draft/${user_id}/${type_id}/${handboek_id}`,
+                  handboek.content_versions
+               )
+            }
+            return {
+               ...handboek,
+               content_versions
+            }
+         }catch(e){
+            throw new Error(e.message)
+         }
+      }
+   }
 })
---
+++ src/store/utils.js
@@ -0,0 +1,66 @@
+import firebase from 'firebase'
+
+export function handboekenRef(user_id, type_id) {
+   return getDocument('handboeken_draft', user_id)
+      .collection('types_documents')
+      .doc(type_id)
+      .collection('documents')
+}
+
+export const getDirectoryFiles = async (path) => {
+   const ref = firebase
+      .storage()
+      .ref()
+   const listRef = ref.child(path)
+   const proxies = await listRef.listAll()
+   return proxies
+}
+
+export const getTxtFileContent = async (path) => {
+   const ref = firebase
+      .storage()
+      .ref()
+   const fileRef = ref.child(`${path}.txt`)
+   try {
+      const url = await fileRef.getDownloadURL()
+      const res = await fetch(url)
+      const txt = await res.text()
+
+      return txt
+   } catch (e) {
+      throw new Error(e.message)
+   }
+}
+
+export const combineVersionsWithStorage = async (path, versions) => {
+   const files = await getDirectoryFiles(path)
+   const filtered = files.items.filter(x => {
+      if (x.name.includes('_') && x.name.includes('.txt')) {
+         const [mayor, sub, minor] = x.name.replace('.txt', '').split('_')
+         const exists = versions.find(y => {
+            if (
+               y.mayor == mayor &&
+               y.sub == sub &&
+               y.minor == minor
+            ) {
+               return x
+            }
+         })
+         if (exists) return x
+      }
+   })
+   const proxies = filtered.map(async x => {
+      const fileName = x.name.replace('.txt', '')
+      const content = await getTxtFileContent(`${path}/${fileName}`)
+      const [mayor, sub, minor] = fileName.split('_')
+      return {
+         mayor: Number(mayor),
+         minor: Number(minor),
+         sub: Number(sub),
+         content
+      }
+   })
+
+   const versionsWithContent = await Promise.all(proxies)
+   return versionsWithContent
+}
