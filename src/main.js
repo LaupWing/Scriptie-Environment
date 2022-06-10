7aec6a3b16b20fca2380089915a9b3ef54a677c7
LaupWing <loc_strict@hotmail.com>
2022-06-10 12:12:26 +0200

    updated

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './index.css'
+import 'default-passive-events'
+import Editor from '@tinymce/tinymce-vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+   render: h => h(App),
+   components: {
+      Editor
+   },
 }).$mount('#app')
