59f0f0b55bf81d8316875f1c9ed723e9108791b2
LaupWing <loc_strict@hotmail.com>
2022-06-11 15:39:39 +0200

    renderd

--- src/components/DocumentViewer/CommentSection/CommentForm/index.vue
+++ src/components/DocumentViewer/CommentSection/CommentForm/index.vue
@@ -1,10 +1,30 @@
 <template>
    <div class="flex">
-      <!-- <textarea 
-         v-model="comment"
-         placeholder="Opmerkingen"
-         class="w-full border bg-gray-100 border-border resize-none rounded focus:outline-none focus:ring-2 focus:ring-highlight p-2 h-20"
-      ></textarea> -->
+      <app-backdrop>
+         <div class="bg-background p-2 m-auto rounded max-w-xl flex flex-col">
+            <header class="flex justify-between">
+               <h2 class="uppercase font-bold text-font">
+                  Gebruikte templates
+               </h2>
+               <icon-close class="w-6"/>
+            </header>
+            <ul 
+               class="bg-main overflow-auto flex flex-col border border-border m-2"
+               :style="{
+                  maxHeight: '70vh'
+               }"
+            >
+               <li
+                  v-for="template in used_templates"
+                  class="p-1 text-sm border-b border-border hover:bg-highlight hover:text-main cursor-pointer duration-200"
+                  :key="template.id"
+               >
+                  <span class="font-semibold">{{template.shortcode}}</span>
+                  {{template.name}}
+               </li>
+            </ul>
+         </div>
+      </app-backdrop>
       <div 
          class="w-full border bg-gray-100 border-border resize-none rounded focus:outline-none focus:ring-2 focus:ring-highlight p-2 h-20"
          contenteditable="true"
@@ -43,6 +63,11 @@ export default {
          comment: ''
       }
    },
+   computed:{
+      used_templates(){
+         return this.templates.filter(x=>this.template_items.templates.includes(x.id))
+      }
+   },
    methods:{
       addComment(){
          console.log(this.comment)
@@ -57,8 +82,7 @@ export default {
       },
    },
    created(){
-      console.log(this.templates.filter(x=>this.template_items.templates.includes(x.id)))
-      console.log(this.template_items)
+      console.log(this.used_templates)
    }
 }
 </script>
